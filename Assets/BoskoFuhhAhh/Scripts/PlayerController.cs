5d0c2ff561f0341e60a2fccfc45af86201ebe388
Destory-Object <Khaledaldroubi2@gmail.com>
2026-01-14 13:36:18 +0100

    fixed code

--- Assets/BoskoFuhhAhh/Scripts/PlayerController.cs
+++ Assets/BoskoFuhhAhh/Scripts/PlayerController.cs
@@ -100,7 +100,7 @@ public class PlayerController : MonoBehaviour
             return;
         }else
         {
-            GetComponent<PlayerDamageReceiver>().SwitchDamageRecieve(true);
+            GetComponent<PlayerDamageReceiver>().SwitchDamageRecieve(true); 
         }
 
 
